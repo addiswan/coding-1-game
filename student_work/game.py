53c549739bde5daff290e569ed8842de9b9e0fe4
luchinofan22 <s1038294@student.knoxschools.org>
2026-02-25 14:49:55 +0000

    import curses, dont know if we need time

--- student_work/game.py
+++ student_work/game.py
@@ -1,11 +1,13 @@
 # Write your game here
 
-game_data = {
+#game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
-}
+#}
 
-def draw_board(screen):
+#def draw_board(screen):
     # Print the board and all game elements using curses
 
+import curses
+import time
 
 # Good Luck!
