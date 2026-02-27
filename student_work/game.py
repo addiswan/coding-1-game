c4630370f256c7059861503ce224a8e99d2a3827
addiswan <s962925@student.knoxschools.org>
2026-02-27 14:57:56 +0000

    added player movement

--- student_work/game.py
+++ student_work/game.py
@@ -25,7 +25,7 @@ game_data = {
 def draw_board(stdscr):
     curses.start_color()
     curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_WHITE, -1)
+    curses.init_pair(1, curses.COLOR_BLACK, 1)
 
     stdscr.clear()
     for y in range(game_data['height']):
@@ -40,8 +40,31 @@ def draw_board(stdscr):
             else:
                 row += game_data['empty']
         stdscr.addstr(y, 0, row, curses.color_pair(1))
-
+    stdscr.addstr(game_data['height'] + 1, 0,
+                  f"Moves Survived: {game_data['player']['score']}",
+                  curses.color_pair(1))
+    stdscr.addstr(game_data['height'] + 2, 0,
+                  "Move with W/A/S/D, Q to quit",
+                  curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()  # pause so player can see board
 
 curses.wrapper(draw_board)
+
+def move_player(key):
+    x = game_data['player']['x']
+    y = game_data['player']['y']
+
+    new_x, new_y = x, y
+    key = key.lower()
+
+    if key == "w" and y > 0:
+        new_y -= 1
+    elif key == "s" and y < game_data['height'] - 1:
+        new_y += 1
+    elif key == "a" and x > 0:
+        new_x -= 1
+    elif key == "d" and x < game_data['width'] - 1:
+        new_x += 1
+    else:
+        return  # Invalid key or move off board
