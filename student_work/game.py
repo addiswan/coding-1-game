4e37367c605a2f45f61a03aac87e8888932132e7
luchinofan22 <s1038294@student.knoxschools.org>
2026-03-04 15:21:49 +0000

    misc minor edits (make chicken move when)

--- student_work/game.py
+++ student_work/game.py
@@ -5,8 +5,8 @@ import time
 
 # Good Luck!
 game_data = {
-    'width': 20,
-    'height': 25,
+    'width': 10,
+    'height': 10,
     'player': {"x": 0, "y": 0, "score": 0},
     'obstacles': [
         {"x": 1, "y": 2},
@@ -18,14 +18,13 @@ game_data = {
     'rock': "\U0001FAA8 ",
     'tree1': "\U0001F332",
     'tree2': "\U0001F333",
-
     'empty': "  "
 }
 
 def draw_board(stdscr):
     curses.start_color()
     curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_BLACK, 1)
+    curses.init_pair(1, curses.COLOR_BLACK, -1)
 
     stdscr.clear()
     for y in range(game_data['height']):
@@ -34,20 +33,20 @@ def draw_board(stdscr):
             # Player
             if x == game_data['player']['x'] and y == game_data['player']['y']:
                 row += game_data['chicken']
-            # Obstacles
-           # elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
-              #  row += game_data['obstacle']
+            #Obstacles
+            elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
+               row += game_data['rock']
             else:
                 row += game_data['empty']
         stdscr.addstr(y, 0, row, curses.color_pair(1)) #read docs for curses
-    stdscr.addstr(game_data['height'] + 1, 0,
-                  f"Moves Survived: {game_data['player']['score']}",
-                  curses.color_pair(0))
-    stdscr.addstr(game_data['height'] + 2, 0,
-                  "Move with W/S/D (no backwards), Q to quit",
-                  curses.color_pair(0))
-    stdscr.refresh()
-    stdscr.getkey()  # pause so player can see board
+        stdscr.addstr(game_data['height'] + 1, 0,
+                    f"Moves Survived: {game_data['player']['score']}",
+                    curses.color_pair(1))
+        stdscr.addstr(game_data['height'] + 2, 0,
+                    "Move with W/S/D (no backwards), Q to quit",
+                    curses.color_pair(1))
+        stdscr.refresh()
+        stdscr.getkey()  # pause so player can see board
 
 curses.wrapper(draw_board)
 
@@ -64,13 +63,13 @@ def move_player(key):
         new_y += 1
     elif key == "d" and x < game_data['width'] - 1:
         new_x += 1
-    else:
-        return  # Invalid key or move off board
+    # else:
+    #     return  # Invalid key or move off board
     
         # Check for obstacles
-    if any(o['x'] == new_x and o['y'] == new_y for o in game_data['obstacles']):
-        return
-
+    # if any(o['x'] == new_x and o['y'] == new_y for o in game_data['obstacles']):
+    #     return
+    
     # Update position and increment score
     game_data['player']['x'] = new_x
     game_data['player']['y'] = new_y
